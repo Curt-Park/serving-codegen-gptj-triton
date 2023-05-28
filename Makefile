3beedd222df21096ee62567b08b15e84818ebac6
Curt-Park <jinwoo.park@annotation-ai.com>
2023-05-28 21:31:18 +0900

    Add Gradio client with triton server

--- Makefile
+++ Makefile
@@ -24,12 +24,21 @@ triton:
 		-p 8000:8000 -p 8001:8001 -p 8002:8002  -v $(PWD)/codegen-350M-mono-gptj/model_repository:/models \
 		$(TRITON_CONTAINER_NAME):$(TRITON_VERSION) tritonserver --model-repository=/models
 
+perf-help:
+	docker run -it --ipc=host --net=host nvcr.io/nvidia/tritonserver:$(TRITON_VERSION)-py3-sdk \
+		/workspace/install/bin/perf_analyzer --help\
+
 perf:
 	docker run -it --ipc=host --net=host nvcr.io/nvidia/tritonserver:$(TRITON_VERSION)-py3-sdk \
 		/workspace/install/bin/perf_analyzer \
 			-m ensemble --percentile=95 \
 			-i grpc -u 0.0.0.0:8001 \
-			--concurrency-range 16:16
+			--concurrency-range 1:1 \
+			--input-data  sample_request_ensemble.json
+			# --shape INPUT_0:32 --shape INPUT_1:1 --shape INPUT_2:1 --shape INPUT_3:1
+
+curl:
+	curl -X POST -H 'Content-Type: application/json' -d @sample_request_ensemble.json http://localhost:8000/v2/models/ensemble/infer
 
 
 # Dev
--- src/client/app.py
+++ src/client/app.py
@@ -3,8 +3,11 @@
 - Author: Curt Park
 - Email: www.jwpark.co.kr@gmail.com
 """
+import os
 import gradio as gr
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, set_seed
+import numpy as np
+import tritonclient.http as httpclient
+from tritonclient.utils import np_to_triton_dtype
 
 example = [
     ["def print_hello_world():", 8, 0.6, 42],
@@ -13,25 +16,67 @@ example = [
     ["def count_words(filename):", 42, 0.6, 42],
     ["def two_sum(nums, target):", 140, 0.6, 55],
 ]
-tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-350M-mono")
-model = AutoModelForCausalLM.from_pretrained("moyix/codegen-350M-mono-gptj")
 
 
+URL = os.getenv("TRITON_SERVER_URL", "localhost:8000")
+tritonclient = httpclient.InferenceServerClient(URL, concurrency=1)
+
+
+def prepare_tensor(name: str, tensor: np.ndarray) -> httpclient.InferInput:
+    """Create a triton input."""
+    infer_input = httpclient.InferInput(
+        name, tensor.shape, np_to_triton_dtype(tensor.dtype)
+    )
+    infer_input.set_data_from_numpy(tensor)
+    return infer_input
+
+
+# pylint: disable=too-many-locals,broad-except
 def code_generation(
-    gen_prompt: str, max_tokens: int, temperature: float = 0.6, seed: int = 42
+    gen_prompt: str, max_tokens: int, temp: float = 0.6, seed: int = 42
 ) -> str:
     """Generate code."""
-    set_seed(seed)
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
-    generated_text = pipe(
-        gen_prompt,
-        do_sample=True,
-        top_p=0.95,
-        temperature=temperature,
-        max_new_tokens=max_tokens,
-        pad_token_id=pipe.tokenizer.eos_token_id,
-    )[0]["generated_text"]
-    return generated_text
+    input0 = [[gen_prompt]]
+    input0_data = np.array(input0).astype(object)
+    output0_len = np.ones_like(input0).astype(np.uint32) * max_tokens
+    bad_words_list = np.array([[""]], dtype=object)
+    stop_words_list = np.array([[""]], dtype=object)
+    runtime_top_k = np.ones([input0_data.shape[0], 1]).astype(np.uint32)
+    runtime_top_p = np.zeros([input0_data.shape[0], 1]).astype(np.float32)
+    beam_search_diversity_rate = np.zeros([input0_data.shape[0], 1]).astype(np.float32)
+    temperature = temp * np.ones([input0_data.shape[0], 1]).astype(np.float32)
+    len_penalty = 1.0 * np.ones([input0_data.shape[0], 1]).astype(np.float32)
+    repetition_penalty = 1.0 * np.ones([input0_data.shape[0], 1]).astype(np.float32)
+    random_seed = seed * np.ones([input0_data.shape[0], 1]).astype(np.uint64)
+    is_return_log_probs = np.ones([input0_data.shape[0], 1]).astype(bool)
+    beam_width = np.ones([input0_data.shape[0], 1]).astype(np.uint32)
+    start_ids = 220 * np.ones([input0_data.shape[0], 1]).astype(np.uint32)
+    end_ids = 50256 * np.ones([input0_data.shape[0], 1]).astype(np.uint32)
+
+    inputs = [
+        prepare_tensor("INPUT_0", input0_data),
+        prepare_tensor("INPUT_1", output0_len),
+        prepare_tensor("INPUT_2", bad_words_list),
+        prepare_tensor("INPUT_3", stop_words_list),
+        prepare_tensor("runtime_top_k", runtime_top_k),
+        prepare_tensor("runtime_top_p", runtime_top_p),
+        prepare_tensor("beam_search_diversity_rate", beam_search_diversity_rate),
+        prepare_tensor("temperature", temperature),
+        prepare_tensor("len_penalty", len_penalty),
+        prepare_tensor("repetition_penalty", repetition_penalty),
+        prepare_tensor("random_seed", random_seed),
+        prepare_tensor("is_return_log_probs", is_return_log_probs),
+        prepare_tensor("beam_width", beam_width),
+        prepare_tensor("start_id", start_ids),
+        prepare_tensor("end_id", end_ids),
+    ]
+
+    try:
+        result = tritonclient.infer("ensemble", inputs)
+        output0 = result.as_numpy("OUTPUT_0")
+    except Exception as exception:
+        print(exception)
+    return output0[0].decode("utf-8").replace("<|endoftext|>", "\t")
 
 
 gr.Interface(
