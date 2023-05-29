04a2f394a163d00296312f0fa7716e1a45627ce2
Curt-Park <jinwoo.park@annotation-ai.com>
2023-05-29 19:58:44 +0900

    Build a chatmot with gradio

--- src/client/app.py
+++ src/client/app.py
@@ -1,52 +1,29 @@
-"""CLI Example for CodeGen-350M.
+"""A simple client for CodeGen-350M.
 
 - Author: Curt Park
 - Email: www.jwpark.co.kr@gmail.com
 """
 import os
+from typing import List, Tuple
 
 import gradio as gr
 import numpy as np
 import tritonclient.grpc as grpcclient
 from tritonclient.utils import np_to_triton_dtype
 
-example = [
-    ["def print_hello_world():", 1024, 3, 0.92, 0.5, 1.0, -1, 5, 42, 1],
-    ["def get_file_size(filepath):", 1024, 3, 0.92, 0.5, 1.0, -1, 1, 42, 1],
-    ["def count_lines(filename):", 1024, 3, 0.92, 0.5, 1.0, -1, 1.16, 42, 1],
-    ["def count_words(filename):", 1024, 3, 0.92, 0.5, 0.3, -1, 1, 42, 1],
-    ["def two_sum(nums, target):", 1024, 3, 0.92, 0.5, 1.0, -1, 1, 60, 1],
-    [
-        "Solve the two sum problem with hash map.",
-        1024,
-        3,
-        0.92,
-        0.5,
-        0.1,
-        -1,
-        1,
-        205,
-        1,
-    ],
-]
-
-
 URL = os.getenv("TRITON_SERVER_URL", "localhost:8001")
 tritonclient = grpcclient.InferenceServerClient(URL)
 
 
-def prepare_tensor(name: str, tensor: np.ndarray) -> grpcclient.InferInput:
-    """Create a triton input."""
-    infer_input = grpcclient.InferInput(
-        name, tensor.shape, np_to_triton_dtype(tensor.dtype)
-    )
-    infer_input.set_data_from_numpy(tensor)
-    return infer_input
+def add_text(history: List[Tuple[str, str]], text: str) -> List[str]:
+    """Add the input text."""
+    history += [(text, None)]
+    return history
 
 
 # pylint: disable=too-many-locals,broad-except
-def code_generation(
-    gen_prompt: str,
+def bot(
+    history: List[Tuple[str]],
     max_tokens: int,
     top_k: int,
     top_p: float,
@@ -57,7 +34,8 @@ def code_generation(
     seed: int,
     beams: int,
 ) -> str:
-    """Generate code."""
+    """Predict."""
+    gen_prompt = history[-1][0]
     input0 = [[gen_prompt]]
     input0_data = np.array(input0).astype(object)
     output0_len = np.ones_like(input0).astype(np.uint32) * max_tokens
@@ -101,79 +79,110 @@ def code_generation(
 
     resp = output0[0].decode("utf-8")
     idx = resp.find("<|endoftext|>")
-    return resp[:idx]
+    resp = resp[:idx]
+    history[-1][1] = "```\n" + resp + """\n```"""
+
+    return history
 
 
-gr.Interface(
-    fn=code_generation,
-    inputs=[
-        gr.Code(lines=10, label="Input code"),
-        gr.Slider(
+def prepare_tensor(name: str, tensor: np.ndarray) -> grpcclient.InferInput:
+    """Create a triton input."""
+    infer_input = grpcclient.InferInput(
+        name, tensor.shape, np_to_triton_dtype(tensor.dtype)
+    )
+    infer_input.set_data_from_numpy(tensor)
+    return infer_input
+
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot([], elem_id="chatbot").style(height=750)
+
+    with gr.Row():
+        txt = gr.Textbox(
+            show_label=False,
+            placeholder="Enter text and press enter",
+        ).style(container=False)
+
+    with gr.Row():
+        n_tokens = gr.Slider(
             minimum=8,
             maximum=1024,
             step=1,
-            value=1024,
+            value=256,
             label="Number of tokens to generate",
-        ),
-        gr.Slider(
+        )
+        top_k = gr.Slider(
             minimum=1,
             maximum=100,
             step=1,
-            value=5,
+            value=3,
             label="Top K",
-        ),
-        gr.Slider(
+        )
+        top_p = gr.Slider(
             minimum=0,
             maximum=1,
             step=0.01,
             value=0.92,
             label="Top P",
-        ),
-        gr.Slider(
+        )
+        beam_diversity = gr.Slider(
             minimum=0,
             maximum=1,
             step=0.01,
             value=0.5,
             label="Beam Search Diversity",
-        ),
-        gr.Slider(
+        )
+        temperature = gr.Slider(
             minimum=0,
             maximum=2.5,
             step=0.1,
             value=0.6,
             label="Temperature",
-        ),
-        gr.Slider(
+        )
+        len_penalty = gr.Slider(
             minimum=-1,
             maximum=1,
             step=0.01,
             value=0.0,
             label="Length Penalty",
-        ),
-        gr.Slider(
+        )
+        rep_penalty = gr.Slider(
             minimum=0,
             maximum=5,
             step=0.01,
             value=1.0,
             label="Repetition Penalty",
-        ),
-        gr.Slider(
+        )
+        seed = gr.Slider(
             minimum=0,
             maximum=1000,
             step=1,
-            value=42,
+            value=128,
             label="Random seed to use for the generation",
-        ),
-        gr.Slider(
+        )
+        beams = gr.Slider(
             minimum=1,
             maximum=100,
             step=1,
             value=1,
             label="Beams",
-        ),
-    ],
-    outputs=gr.Code(label="Predicted code", lines=10),
-    allow_flagging="never",
-    examples=example,
-    title="CodeGen Generator",
-).queue().launch()
+        )
+
+    txt.submit(add_text, [chatbot, txt], chatbot,).then(
+        bot,
+        [
+            chatbot,
+            n_tokens,
+            top_k,
+            top_p,
+            beam_diversity,
+            temperature,
+            len_penalty,
+            rep_penalty,
+            seed,
+            beams,
+        ],
+        chatbot,
+    )
+
+demo.queue().launch()
